129edb0d2f6b81fb5a504733cf1db448ecf94044
asus <asus@Bala>
2024-07-01 10:01:30 +0530

    **

---
+++ javaday1/src/javaday1/Identifiers.java
@@ -0,0 +1,21 @@
+package javaday1;
+
+public class Identifiers {
+
+	public static void main(String[] args) {
+		int num1=13;
+		System.out.println("value of the number variable is:"+num1);
+		int num2=13;
+		System.out.println("value of the number variable is:"+num2);
+		int num3=10;
+		int num4=20;
+		System.out.println("value of the number variable is:"+num4);
+		String studentName="Aniket";
+		System.out.println("value is:"+studentName);
+	
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
